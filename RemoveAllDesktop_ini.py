30a458fcd44f18e2f79e83c9365cb5da1100f0df
Haotian2006 <71573739+haotian2006@users.noreply.github.com>
2023-05-08 11:33:39 -0700

    a

---
+++ RemoveAllDesktop_ini.py
@@ -0,0 +1,10 @@
+import os
+# destroy are desktop.ini cause they break github
+current_dir = os.getcwd()
+parent_dir = os.path.dirname(current_dir)
+for dirpath, dirnames, filenames in os.walk(parent_dir):
+    for filename in filenames:
+        if filename == "desktop.ini":
+            file_path = os.path.join(dirpath, filename)
+            os.remove(file_path)
+            print(f"Deleted file: {file_path}")
