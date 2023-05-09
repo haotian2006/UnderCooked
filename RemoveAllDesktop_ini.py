64727faf3402ea62bb078459398bc85458a9665d
Haotian2006 <haotian2090@gmail.com>
2023-05-08 18:17:29 -0700

    a

--- RemoveAllDesktop_ini.py
+++ RemoveAllDesktop_ini.py
@@ -1,6 +1,6 @@
 import os
 # ignore this
-# this is to destroy are desktop.ini cause they break github
+# this is to destroy desktop.ini files that were create by google drive which breaks github when I try to sync it 
 current_dir = os.getcwd()
 parent_dir = os.path.dirname(current_dir)
 for dirpath, dirnames, filenames in os.walk(parent_dir):
